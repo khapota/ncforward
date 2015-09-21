0a201ade30f5c7a97a81d87356dc0f92d07ea68b
Phan Trọng Khanh <khanhpt@appota.com>
2015-09-22 00:13:56 +0700

    Add preference items for encrypt function

--- iOS-client/ncfprefs/ncfprefs.mm
+++ iOS-client/ncfprefs/ncfprefs.mm
@@ -8,9 +8,34 @@
 - (id)specifiers {
 	if(_specifiers == nil) {
 		_specifiers = [[self loadSpecifiersFromPlistName:@"ncfprefs" target:self] retain];
+    if ([[self readPreferenceValue:[self specifierForID:@"enableEncrypted"]] intValue] == 1) {
+      [((PSSpecifier *)[self specifierForID:@"secretkey"]) setProperty:@(TRUE) forKey:@"enabled"];
+      [self reloadSpecifierID:@"secretkey"];
+    }
 	}
 	return _specifiers;
 }
+
+//Enable secret cell if turn on
+- (void)enable: (NSNumber *)enabled forSpecifier: (PSSpecifier *)spec {
+  [self setPreferenceValue:enabled specifier:spec];
+  [[NSUserDefaults standardUserDefaults] synchronize];
+  [((PSSpecifier *)[self specifierForID:@"secretkey"]) setProperty:@(([enabled intValue] == 1)) forKey:@"enabled"];
+  [self reloadSpecifierID:@"secretkey"];
+}
+
+//Correct some cells after loaded
+/*
+-(void)viewDidLoad {
+
+  [super viewDidLoad];
+
+  if ([[self readPreferenceValue:[self specifierForID:@"enableEncrypted"]] intValue] == 1) {
+    [((PSSpecifier *)[self specifierForID:@"secretkey"]) setProperty:@(TRUE) forKey:@"enabled"];
+    [self reloadSpecifierID:@"secretkey"];
+  }
+}
+*/
 
 - (void)twitter {
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.twitter.com/H6nry_/"]];
